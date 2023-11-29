41c166ebdfd355edd485068cb4346f538aea3f3b
ballack <rodrigue-dev>
2023-11-29 16:26:08 +0100

    first commit

--- database/migrations/2023_11_28_081806_add_colunm_fixture.php
+++ database/migrations/2023_11_28_081806_add_colunm_fixture.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('fixtures', function (Blueprint $table) {
-            $table->foreign('team_home_id')->references('team_id')->on('teams');
-            $table->foreign('team_away_id')->references('team_id')->on('teams');
+           // $table->foreign('team_home_id')->references('team_id')->on('teams');
+           // $table->foreign('team_away_id')->references('team_id')->on('teams');
             $table->string('day_timestamp')->nullable();
         });
         Schema::table('stadings', function (Blueprint $table) {
-            $table->foreign('league_id')->references('league_id')->on('leagues');
-            $table->foreign('team_id')->references('team_id')->on('teams');
+            //$table->foreign('league_id')->references('league_id')->on('leagues');
+            //$table->foreign('team_id')->references('team_id')->on('teams');
         });
     }
 
