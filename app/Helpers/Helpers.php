c520d4bf6626e21c52376bc826d84def8a23bafa
ballack <rodrigue-dev>
2024-02-22 08:49:34 +0100

    ertres

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -194,7 +194,7 @@ class Helpers
             $lastgameafter = Fixture::query()->firstWhere(function (Builder $builder) use ($item, $team_id) {
                 $builder->where('day_timestamp', '>', $item->day_timestamp)
                     ->where('team_home_id', '=', $team_id)
-                    ->orWhere('team_away_id', '=', $team_id)->orderBy('day_timestamp', 'desc');
+                    ->orWhere('team_away_id', '=', $team_id)->orderBy('day_timestamp', 'asc');
             });
             if ($lastgameafter instanceof Fixture) {
                 logger($lastgameafter->fixture_id . ': score after lost' . $lastgameafter->score_ft_home . '-' . $lastgameafter->score_ft_away);
